1519c0064c65d1f12afd7771a9479e6be6e39408
Bryan Ricker <bricker88@gmail.com>
2014-03-19 15:46:36 -0700

    Add autoload path for rails 3

--- lib/outpost/secretary/engine.rb
+++ lib/outpost/secretary/engine.rb
@@ -1,6 +1,9 @@
 module Outpost
   module Secretary
     class Engine < ::Rails::Engine
+      # This is necessary to support rails 3, which doesn't autoload
+      # the concerns directory
+      config.autoload_paths << File.expand_path("../../../../app/controllers/concerns", __FILE__)
     end
   end
 end
