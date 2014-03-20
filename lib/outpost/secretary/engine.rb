ba5bbe0362fd01f6c70f69693c26b39e2acabb33
Bryan Ricker <bricker88@gmail.com>
2014-03-19 17:39:58 -0700

    Move initializer to engine.rb

--- lib/outpost/secretary/engine.rb
+++ lib/outpost/secretary/engine.rb
@@ -3,7 +3,20 @@ module Outpost
     class Engine < ::Rails::Engine
       # This is necessary to support rails 3, which doesn't autoload
       # the concerns directory
-      config.autoload_paths << File.expand_path("../../../../app/controllers/concerns", __FILE__)
+      config.autoload_paths << File.expand_path(
+        "../../../../app/controllers/concerns", __FILE__)
+
+      Rails.application.config.to_prepare do
+        # Make versions an outpost model so it can be represented in Outpost.
+        ::Secretary::Version.instance_eval do
+          outpost_model
+        end
+
+        # Add user injection callback to resource controller.
+        Outpost::ResourceController.instance_eval do
+          include Outpost::Secretary::UserInjection
+        end
+      end
     end
   end
 end
